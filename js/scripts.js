2f776e9b5b29996ec72d375557ef3524b7b57d92
nicolasxavierdev <nicolasxavier.dev@gmail.com>
2021-03-22 14:50:29 -0300

    console.log

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,22 @@
 // tipos de variaveis
 
-let name = "Nícolas";
+// let name = "Nícolas";
 
-let number = 10;
+// let number = 10;
 
-let boolean = tru / false;
+// let boolean = true;
 
-let float = 1.5;
+// let boolean = false;
+
+// let float = 1.5;
 
 
+// concatenção
 
+let nome = "José";
+
+let sobrenome = "Maria";
+
+let idade = 34;
+
+console.log (`Ola! Meu nome é ${nome} ${sobrenome} e eu tenho ${idade} anos`);
