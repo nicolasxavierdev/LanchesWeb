0b4556021176b5c7298501d48a21c80c85aa6a48
nicolasxavierdev <nicolasxavier.dev@gmail.com>
2021-04-19 14:32:37 -0300

    correção

--- js/scripts.js
+++ js/scripts.js
@@ -317,7 +317,7 @@ adicionarCarro({
 
 
 // Função 6
-function calcularMedia(nota1, nota2, nota3, nota4) {
+function calcularMedia(nota1 = 0, nota2 = 0, nota3 = 0, nota4 = 0) {
    let media = (nota1 + nota2 + nota3 + nota4) / 4;
 
    if (media >= 6) {
@@ -329,5 +329,5 @@ function calcularMedia(nota1, nota2, nota3, nota4) {
    }
 }
 
-calcularMedia(5, 6, 6, 7)
+calcularMedia()
 
