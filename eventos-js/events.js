7009b92ab404c92431d237dfd9a604d81b31dd1c
nicolasxavierdev <nicolasxavier.dev@gmail.com>
2021-04-22 15:53:12 -0300

    Evento 3

--- eventos-js/events.js
+++ eventos-js/events.js
@@ -21,13 +21,13 @@ function render() {
 
     botao.addEventListener('click', () => {
 
-        let nota1 = parseInt(prompt('Digite a primeira nota.'))
+        /*let nota1 = parseInt(prompt('Digite a primeira nota.'))
         let nota2 = parseInt(prompt('Digite a segunda nota.'))
         let nota3 = parseInt(prompt('Digite a terceira nota.'))
         let nota4 = parseInt(prompt('Digite a quarta nota.'))
-
+        
         let media = (nota1 + nota2 + nota3 + nota4) / 4;
-
+        
         if (media >= 6) {
             alert(`Média ${media}, parabéns! Aluno aprovado.`);
         } else if (media > 5 && media < 5.9) {
@@ -35,5 +35,13 @@ function render() {
         } else {
             alert(`Média ${media}, aluno reprovado.`);
         }
+        })
+        } */
+
+
+        // Evento 3
+
+        let agora = new Date()    
+        alert(agora)
     })
 }
