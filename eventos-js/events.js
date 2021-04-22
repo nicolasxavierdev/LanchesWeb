040a90785e3f2cf5c8da7eed7c8aec3e81828c96
nicolasxavierdev <nicolasxavier.dev@gmail.com>
2021-04-22 15:11:39 -0300

    Evento 2

--- eventos-js/events.js
+++ eventos-js/events.js
@@ -1,6 +1,6 @@
 // Evento 1
 
- window.addEventListener('load', render)
+/* window.addEventListener('load', render)
 
 function render() {
     let botao = document.querySelector('button#enviar')
@@ -9,6 +9,31 @@ function render() {
 
         alert('Botão foi clicado!')
     })
-} 
+} */
 
 
+// Evento 2
+
+window.addEventListener('load', render)
+
+function render() {
+    let botao = document.querySelector('button#enviar')
+
+    botao.addEventListener('click', () => {
+
+        let nota1 = parseInt(prompt('Digite a primeira nota.'))
+        let nota2 = parseInt(prompt('Digite a segunda nota.'))
+        let nota3 = parseInt(prompt('Digite a terceira nota.'))
+        let nota4 = parseInt(prompt('Digite a quarta nota.'))
+
+        let media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        if (media >= 6) {
+            alert(`Média ${media}, parabéns! Aluno aprovado.`);
+        } else if (media > 5 && media < 5.9) {
+            alert(`Média ${media}, puts! Aluno será encaminhado para conselho de classe`);
+        } else {
+            alert(`Média ${media}, aluno reprovado.`);
+        }
+    })
+}
